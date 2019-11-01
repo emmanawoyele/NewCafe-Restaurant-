b414547de2908e913acb307801a166e284d17e30
Emmanuel Awoyele <levos@DESKTOP-GBMVQ19.localdomain>
2019-11-01 00:09:07 +0000

    Production to the form

---
+++ assets/js/NewCafe.js
@@ -0,0 +1,23 @@
+
+
+/*document.querySelector('.menu-container').addEventListener('click', function(){
+ document.querySelector('.pop-out1').style.display="block";
+    
+});
+document.querySelector('.menu-container').addEventListener('click', function(){
+    document.querySelector('.body-opacity').style.display='block';
+    
+});
+
+document.querySelector('.close-sign').addEventListener('click', function(){
+    document. querySelector(".pop-out1").style.display='none';
+   
+
+});
+document.querySelector('.close-sign').addEventListener('click', function(){
+    document.querySelector('.body-opacity').style.display='none';
+});
+
+ */ 
+ var textslider = document.querySelector(".testi-container");
+ var items = document. querySelectorAll(".items");
