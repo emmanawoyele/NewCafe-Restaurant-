5fee0397119384f915e0aa9156bfa3b39dc18bf8
Emmanuel Awoyele <levos@DESKTOP-GBMVQ19.localdomain>
2019-11-12 21:35:51 +0000

    final production

--- assets/js/NewCafe.js
+++ assets/js/NewCafe.js
@@ -1,23 +1,51 @@
-
-
-/*document.querySelector('.menu-container').addEventListener('click', function(){
- document.querySelector('.pop-out1').style.display="block";
-    
-});
+/*
 document.querySelector('.menu-container').addEventListener('click', function(){
-    document.querySelector('.body-opacity').style.display='block';
+  document.querySelector('.pop-out1').style.display="block";
+     
+ });
+ document.querySelector('.menu-container').addEventListener('click', function(){
+  document.querySelector('.body-opacity').style.display='block';
+  
+});
+ 
+ document.querySelector('.close-sign').addEventListener('click', function(){
+     document. querySelector(".pop-out1").style.display='none';
     
-});
+ 
+ });
+ document.querySelector('.close-sign').addEventListener('click', function(){
+     document.querySelector('.body-opacity').style.display='none';
+ });
+ */
 
-document.querySelector('.close-sign').addEventListener('click', function(){
-    document. querySelector(".pop-out1").style.display='none';
+var yesnow=document.querySelectorAll('.menu-container');
+
+var mens = Array.from(yesnow);
+
+
+var customNav = document.querySelector(".navbar-container");
+var mylinks = document.querySelectorAll(".nav-link");
+var CustomSticky = customNav.offsetTop;
+
+
+function myNav(){
+    
+    console.log("CustomSticy= " + CustomSticky);
+    console.log("scrollY=" + window.scrollY);
+  
+    
+    
+  if (window.scrollY >= CustomSticky){
+ 
+    customNav.classList.add("sticky-navbar")
    
+    
 
-});
-document.querySelector('.close-sign').addEventListener('click', function(){
-    document.querySelector('.body-opacity').style.display='none';
-});
+  }else{
+     
+    customNav.classList.remove("sticky-navbar");
+    
+  }
+}   
+window.addEventListener('scroll', myNav)
 
- */ 
- var textslider = document.querySelector(".testi-container");
- var items = document. querySelectorAll(".items");
