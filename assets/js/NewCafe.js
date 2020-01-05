d72fb52feb79d561443153e7fa6934ee2e1906fd
Emmanuel Awoyele <levos@DESKTOP-GBMVQ19.localdomain>
2020-01-05 14:39:49 +0000

    Brand name changed

--- assets/js/NewCafe.js
+++ assets/js/NewCafe.js
@@ -1,22 +1,25 @@
-/*
-document.querySelector('.menu-container').addEventListener('click', function(){
-  document.querySelector('.pop-out1').style.display="block";
-     
- });
- document.querySelector('.menu-container').addEventListener('click', function(){
-  document.querySelector('.body-opacity').style.display='block';
-  
-});
- 
- document.querySelector('.close-sign').addEventListener('click', function(){
-     document. querySelector(".pop-out1").style.display='none';
+
+function imagePopOut(){
+  document.querySelector('.menu-container').addEventListener('click', function(){
+    document.querySelector('.pop-out1').style.display="block";
+       
+   });
+   document.querySelector('.menu-container').addEventListener('click', function(){
+    document.querySelector('.body-opacity').style.display='block';
     
- 
- });
- document.querySelector('.close-sign').addEventListener('click', function(){
-     document.querySelector('.body-opacity').style.display='none';
- });
- */
+  });
+   
+   document.querySelector('.close-sign').addEventListener('click', function(){
+       document. querySelector(".pop-out1").style.display='none';
+      
+   
+   });
+   document.querySelector('.close-sign').addEventListener('click', function(){
+       document.querySelector('.body-opacity').style.display='none';
+   });
+   
+}
+
 
 
 var icons =document.getElementsByClassName('fas');
